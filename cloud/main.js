545605317558b2bbb6c77f9762be2ad75c28873a
Brian Dooley <brian.dooley@feedhenry.com>
2014-06-19 15:32:16 +0100

    tidy up

--- cloud/main.js
+++ cloud/main.js
@@ -1,44 +1,49 @@
-//var util = require('util');
 /* main.js
  * All calls here are publicly exposed as REST API endpoints.
- * - all parameters must be passed in a single JSON paramater.
  * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
 */
 var appurl = 'https://support-tbd8odimttbsvkhssiqdij3l-dev.df.dev.e111.feedhenry.net/cloud/'
+// Detail of this request module can be found at https://github.com/mikeal/request
 var request = require('request');
 
 exports.list = function(params, callback) {
-// Detail of this request module can be found at https://github.com/mikeal/request
-//var request = require('request');
 url = appurl + 'fhdbList';
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log("Response is " + body);
     return callback(undefined, body);
   }
+  else {
+    console.log("ERROR.  Response is " + error);
+    return callback(undefined, error);
+  }
 })
 };
 
 exports.deleteall = function(params, callback) {
-// Detail of this request module can be found at https://github.com/mikeal/request
-//var request = require('request');
 url = appurl + 'fhdbDeleteall';
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log("Response is " + body);
     return callback(undefined, body);
   }
+  else {
+    console.log("ERROR.  Response is " + error);
+    return callback(undefined, error);
+  }
 })
 };
 
 exports.add = function(params, callback) {
-// Detail of this request module can be found at https://github.com/mikeal/request
-//var request = require('request');
 url = appurl + 'fhdbAdd?firstname=jim&lastname=jones&country=Ireland&phone=123456';
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log("Response is " + body);
     return callback(undefined, body);
+  }
+  else {
+    console.log("ERROR.  Response is " + error);
+    return callback(undefined, error);
   }
 })
 };
